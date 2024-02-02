0736cf5145cb19961b06ed6f9fe674c3484f7ca9
michael sulistio <michaelsulistio@gmail.com>
2024-02-01 17:07:28 -0800

    [test] Working stubbed AndroidManifest modifier to add activity

--- plugin/src/withAndroidActivity.ts
+++ plugin/src/withAndroidActivity.ts
@@ -10,7 +10,71 @@ import path from "path";
 const withAndroidActivity: ConfigPlugin = (config) => {
   console.log("Running withAndroidActivity");
   config = withAndroidManifest(config, async (config) => {
-    // AndroidManifest modification logic here
+    // <activity
+    //     android:name=".MobileWalletAdapterBottomSheetActivity"
+    //     android:launchMode="singleTask"
+    //     android:theme="@style/Theme.ExampleWallet.BottomSheet"
+    //     android:layout_gravity="bottom"
+    //     android:exported="true">
+    //     <!-- Default solana-wallet URI from a browser or native dapp -->
+    //     <intent-filter android:order="1">
+    //         <action android:name="android.intent.action.VIEW" />
+    //         <category android:name="android.intent.category.DEFAULT" />
+    //         <category android:name="android.intent.category.BROWSABLE" />
+    //         <data android:scheme="solana-wallet" />
+    //     </intent-filter>
+    //     <!-- Any other uncategorized solana-wallet URI not covered by above -->
+    //     <intent-filter android:order="0">
+    //         <category android:name="android.intent.category.DEFAULT" />
+    //         <data android:scheme="solana-wallet" />
+    //     </intent-filter>
+    // </activity>
+
+    const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(
+      config.modResults
+    );
+
+    // Ensure the application's activity array is initialized
+    if (!Array.isArray(mainApplication.activity)) {
+      mainApplication.activity = [];
+    }
+
+    // Check if the activity already exists to avoid duplicates
+    if (
+      !mainApplication.activity.find(
+        (item) =>
+          item.$["android:name"] === ".MobileWalletAdapterBottomSheetActivity"
+      )
+    ) {
+      mainApplication.activity.push({
+        $: {
+          "android:name": ".MobileWalletAdapterBottomSheetActivity",
+          "android:launchMode": "singleTask",
+          "android:theme": "@style/Theme.ExampleWallet.BottomSheet",
+          "android:layout_gravity": "bottom",
+          "android:exported": "true",
+        },
+        "intent-filter": [
+          {
+            // $: { "android:order": "1" },
+            action: [{ $: { "android:name": "android.intent.action.VIEW" } }],
+            category: [
+              { $: { "android:name": "android.intent.category.DEFAULT" } },
+              { $: { "android:name": "android.intent.category.BROWSABLE" } },
+            ],
+            data: [{ $: { "android:scheme": "solana-wallet" } }],
+          },
+          {
+            // $: { "android:order": "0" },
+            category: [
+              { $: { "android:name": "android.intent.category.DEFAULT" } },
+            ],
+            data: [{ $: { "android:scheme": "solana-wallet" } }],
+          },
+        ],
+      });
+    }
+
     return config;
   });
 
@@ -22,39 +86,41 @@ const withActivitySourceCode: ConfigPlugin = (config) => {
   return withDangerousMod(config, [
     "android",
     (config) => {
+      const projectRoot = config.modRequest.projectRoot;
       const packageName = AndroidConfig.Package.getPackage(config);
-
       if (!packageName) {
-        throw Error("Android project package name not found.");
+        throw Error("Unable to find Android package name");
       }
 
-      const activityPath = `android/app/src/main/java/${packageName.replace(
+      const activitySourcePath = path.join(
+        projectRoot,
+        "MobileWalletAdapterBottomSheetActivity.kt"
+      );
+
+      console.log(
+        "Attempting to copy activity source at: " + activitySourcePath
+      );
+
+      const activityDestinationPath = `android/app/src/main/java/${packageName.replace(
         /\./g,
         "/"
-      )}/MyNewActivity.java`;
-      const activityJavaCode = `package ${packageName};
-            
-            import android.os.Bundle;
-            import androidx.appcompat.app.AppCompatActivity;
-            
-            public class MyNewActivity extends AppCompatActivity {
-                @Override
-                protected void onCreate(Bundle savedInstanceState) {
-                    super.onCreate(savedInstanceState);
-                    // setContentView(R.layout.activity_my_new);
-                }
-            }`;
+      )}/MobileWalletAdapterBottomSheetActivity.kt`;
 
-      // Ensure the directory exists
-      const activityDirectory = path.dirname(activityPath);
+      // Ensure the directory exists for the destination
+      const activityDirectory = path.dirname(activityDestinationPath);
       if (!fs.existsSync(activityDirectory)) {
-        console.log("activity directory didn't exist");
         fs.mkdirSync(activityDirectory, { recursive: true });
       }
 
-      // Write the activity source code to the file
-      console.log("Writing activity source code to path: " + activityPath);
-      fs.writeFileSync(activityPath, activityJavaCode);
+      // Copy the file
+      if (fs.existsSync(activitySourcePath)) {
+        fs.copyFileSync(activitySourcePath, activityDestinationPath);
+      } else {
+        console.error(
+          `Could not find MobileWalletAdapterBottomSheetActivity.kt at ${activitySourcePath}`
+        );
+        // Consider whether to throw an error or not
+      }
 
       return config;
     },
