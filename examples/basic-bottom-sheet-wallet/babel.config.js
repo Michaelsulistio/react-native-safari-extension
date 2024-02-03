2a5ab41d1b59487f84b2c8c2eba8bd8b0255f061
michael sulistio <michaelsulistio@gmail.com>
2024-02-02 16:09:50 -0800

    [alpha] Add example folder + basic example stubbed

--- examples/basic-bottom-sheet-wallet/babel.config.js
+++ examples/basic-bottom-sheet-wallet/babel.config.js
@@ -1,19 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
-  let plugins = [];
-  
-  
-
-  
-
-  
-    plugins.push('expo-router/babel');
-  
-
-  
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: plugins,
   };
 };
